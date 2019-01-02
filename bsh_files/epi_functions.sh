c4ba25c5f628ed0c52a0b5979dae50d2cb579aae
Fantin Bibas <fantin.bibas@epitech.eu>
2019-01-02 23:54:11 +0100

    Add epirepo1 for second blih account

--- bsh_files/epi_functions.sh
+++ bsh_files/epi_functions.sh
@@ -2,3 +2,8 @@ epirepo() {
     blih repository create "$1"
     blih repository setacl "$1" ramassage-tek r
 }
+
+epirepo1() {
+    blih1 repository create "$1"
+    blih1 repository setacl "$1" ramassage-tek r
+}
